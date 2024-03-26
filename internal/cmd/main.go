90a5fe8fa64989f44fda18784414f53bd5fc358c
Lorena Valle <lore1268valle@gmail.com>
2024-03-25 21:02:10 -0600

    Function ConnectToDB created to connect with Database

--- internal/cmd/main.go
+++ internal/cmd/main.go
@@ -1,6 +1,12 @@
 package main
 
+import (
+	"github.com/encuestas-go/back-enc/internal/database"
+)
+
 func main() {
 	Build()
+
+	surveyDB := database.ConnectToDB()
 
 }
--- internal/database/database.go
+++ internal/database/database.go
@@ -1,1 +1,31 @@
 package database
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+func ConnectToDB() *sql.DB {
+
+	cfg := mysql.Config{
+		User:   "",
+		Passwd: "",
+		Addr:   "",
+		DBName: "ENCUESTAS",
+		Net:    "tcp",
+	}
+
+	db, err := sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatalf("Cannot connect to database, error founded: %v", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Couldn't ping with the database, error detected: %v", err)
+	}
+
+	return db
+}
