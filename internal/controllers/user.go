e7d8055b29b8b675e34a4b79161f4713a4afbc4c
Lorena Valle <lore1268valle@gmail.com>
2024-03-27 18:55:48 -0600

    Finished User Controller

--- internal/controllers/user.go
+++ internal/controllers/user.go
@@ -1,8 +1,12 @@
 package controllers
 
-import "github.com/labstack/echo/v4"
+import (
+	"net/http"
 
-// UserController its a struct to
+	"github.com/labstack/echo/v4"
+)
+
+// UserController its a struct for User
 type UserController struct{}
 
 func InitUserController() *UserController {
@@ -10,6 +14,17 @@ func InitUserController() *UserController {
 }
 
 func (u *UserController) CreateUser(c echo.Context) error {
+	return c.JSON(200, "User succesfully created ")
+}
 
-	return c.JSON(200, "")
+func (u *UserController) UpdateUser(c echo.Context) error {
+	return c.JSON(http.StatusOK, "User succesfully updated")
+}
+
+func (u *UserController) DeleteUser(c echo.Context) error {
+	return c.JSON(http.StatusOK, "User succesfully deleted")
+}
+
+func (u *UserController) GetUser(c echo.Context) error {
+	return c.JSON(http.StatusOK, "Here's the user selected")
 }
--- internal/server/router.go
+++ internal/server/router.go
@@ -8,11 +8,88 @@ func (s *ServerHandler) StartRoutes() *ServerHandler {
 
 // InitalizeUserRoutes creates the routes for the user requirement based on the group: /v1/api.
 // The routes are:
-// - /crear/usuario
+//   - /crear/usuario
+//     /actualizar/usuario
+//     /eliminar/usuario
+//     /consultar/usuario
 func (s *ServerHandler) InitalizeUserRoutes() *ServerHandler {
 	s.RouterGroup.POST("/crear/usuario", s.UserController.CreateUser)
-	s.RouterGroup.PUT("/actualizar/usuario", nil)
-	s.RouterGroup.DELETE("/eliminar/usuario", nil)
-
+	s.RouterGroup.PUT("/actualizar/usuario", s.UserController.UpdateUser)
+	s.RouterGroup.DELETE("/eliminar/usuario", s.UserController.DeleteUser)
+	s.RouterGroup.GET("/consultar/usuario", s.UserController.GetUser)
 	return s
 }
+
+/*
+func (s *ServerHandler) StartSocioeconomicStatusRoutes() *ServerHandler {
+	s.RouterGroup.POST("/crear/nivelSocioeconomico",)
+	s.RouterGroup.PUT("/actualizar/nivelSocioeconomico",)
+	s.RouterGroup.DELETE("/eliminar/nivelSocioeconomico",)
+	s.RouterGroup.GET("/consultar/nivelSocioeconomico",)
+	return s
+}
+
+func (s *ServerHandler) StartEconomicStatusRoutes() *ServerHandler {
+	s.RouterGroup.POST("/crear/nivelEconomico",)
+	s.RouterGroup.PUT("/crear/nivelEconomico",)
+	s.RouterGroup.DELETE("/crear/nivelEconomico",)
+	s.RouterGroup.GET("/crear/nivelEconomico",)
+	return s
+}
+
+func (s *ServerHandler) StartTransportManagementRoutes() *ServerHandler {
+	s.RouterGroup.POST("/crear/medioTransporte",)
+	s.RouterGroup.PUT("/actualizar/medioTransporte",)
+	s.RouterGroup.DELETE("/eliminar/medioTransporte",)
+	s.RouterGroup.GET("/consultar/medioTransporte",)
+	return s
+}
+
+func (s *ServerHandler) StartHouseholdInfrastructureRoutes() *ServerHandler {
+	s.RouterGroup.POST("/crear/InfraestructuraCasa",)
+	s.RouterGroup.PUT("/actualizar/InfraestructuraCasa",)
+	s.RouterGroup.DELETE("/eliminar/InfraestructuraCasa",)
+	s.RouterGroup.GET("/consultar/InfraestructuraCasa",)
+	return s
+}
+
+func (s *ServerHandler) StartDemographicManagementRoutes() *ServerHandler {
+	s.RouterGroup.POST("/crear/nivelDemografico",)
+	s.RouterGroup.PUT("/actualizar/nivelDemografico",)
+	s.RouterGroup.DELETE("/eliminar/nivelDemografico",)
+	s.RouterGroup.GET("/consultar/nivelDemografico",)
+	return s
+}
+
+func (s *ServerHandler) StartActivitiesManagementRoutes() *ServerHandler {
+	s.RouterGroup.POST("/crear/actividad",)
+	s.RouterGroup.PUT("/actualizar/actividad",)
+	s.RouterGroup.DELETE("/eliminar/actividad",)
+	s.RouterGroup.GET("/consultar/actividad",)
+	return s
+}
+
+func (s *ServerHandler) StartServiceManagementRoutes() *ServerHandler {
+	s.RouterGroup.POST("/crear/servicio",)
+	s.RouterGroup.PUT("/actualizar/servicio",)
+	s.RouterGroup.DELETE("/eliminar/servicio",)
+	s.RouterGroup.GET("/consultar/servicio",)
+	return s
+}
+
+func (s *ServerHandler) StartEventManagementRoutes() *ServerHandler {
+	s.RouterGroup.POST("/crear/evento",)
+	s.RouterGroup.PUT("/actualizar/evento",)
+	s.RouterGroup.DELETE("/eliminar/evento",)
+	s.RouterGroup.GET("/consultar/evento",)
+	return s
+}
+
+func (s *ServerHandler) StartSatisfactorySurveysRoutes() *ServerHandler {
+	s.RouterGroup.POST("/crear/encuestaSatisfaccion",)
+	s.RouterGroup.PUT("/actualizar/encuestaSatisfaccion",)
+	s.RouterGroup.DELETE("/eliminar/encuestaSatisfaccion",)
+	s.RouterGroup.GET("/consultar/encuestaSatisfaccion",)
+	return s
+}
+*/
