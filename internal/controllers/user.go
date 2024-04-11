ea59cb7c0529a7404c2c54ab1c1a8d0b01c016a9
Lorena Valle <lore1268valle@gmail.com>
2024-04-11 14:29:52 -0600

    Functions Update and Delete done on User controller and User Repository

--- internal/controllers/user.go
+++ internal/controllers/user.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/encuestas-go/back-enc/internal/domain"
 	"github.com/encuestas-go/back-enc/internal/repository"
@@ -59,12 +60,63 @@ func (u *UserController) Update(c echo.Context) error {
 	// que llega:
 	// body del usuario
 	// parametro del request
-	return c.JSON(http.StatusOK, "User succesfully updated")
+	user := domain.User{}
+	userID := c.QueryParam("user_id")
+	userIDConverted, err := strconv.Atoi(userID)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, ControllerMessageResponse{
+			StatusCode: http.StatusBadRequest,
+			Message:    fmt.Sprintf("Invalid userID requested: %v", err),
+		})
+	}
+
+	err = c.Bind(&user)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, ControllerMessageResponse{
+			StatusCode: http.StatusInternalServerError,
+			Message:    fmt.Sprintf("An error happening trying to bind the body, err: %v", err),
+		})
+	}
+
+	err = u.UserRepository.Update(user, userIDConverted)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, ControllerMessageResponse{
+			StatusCode: http.StatusInternalServerError,
+			Message:    fmt.Sprintf("An error occurred while trying to update the user: %v", err),
+		})
+	}
+
+	return c.JSON(http.StatusCreated, ControllerMessageResponse{
+		StatusCode: http.StatusCreated,
+		Message:    fmt.Sprintf("User with ID %s successfully updated", userID),
+	})
+	//return c.JSON(http.StatusOK, "User succesfully updated")
 }
 
 func (u *UserController) Delete(c echo.Context) error {
 	// paramatro del request: id del usuario
-	return c.JSON(http.StatusOK, "User succesfully deleted")
+	user := domain.User{}
+	userID := c.QueryParam("user_id")
+	userIDConverted, err := strconv.Atoi(userID)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, ControllerMessageResponse{
+			StatusCode: http.StatusBadRequest,
+			Message:    fmt.Sprintf("Invalid userID requested: %v", err),
+		})
+	}
+	err = u.UserRepository.Delete(user, userIDConverted)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, ControllerMessageResponse{
+			StatusCode: http.StatusInternalServerError,
+			Message:    fmt.Sprintf("An error occurred while trying to delete the user: %v", err),
+		})
+	}
+
+	return c.JSON(http.StatusCreated, ControllerMessageResponse{
+		StatusCode: http.StatusCreated,
+		Message:    fmt.Sprintf("User with ID %s successfully deleted", userID),
+	})
+	//return c.JSON(http.StatusOK, "User succesfully deleted")
 }
 
 func (u *UserController) Get(c echo.Context) error {
