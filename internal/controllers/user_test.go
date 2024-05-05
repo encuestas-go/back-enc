435a33378b5543a8b04169a9c5bda5fb797a4a4e
Lorena Valle <lore1268valle@gmail.com>
2024-05-04 18:23:01 -0600

    Test of create user done

--- internal/controllers/user_test.go
+++ internal/controllers/user_test.go
@@ -1,48 +1,91 @@
 package controllers
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
 
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/encuestas-go/back-enc/internal/domain"
+	"github.com/encuestas-go/back-enc/internal/repository"
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
-)
-
-var (
-	userJSON = `{
-		"name" : "Lorena ",
-		"middle_name" : "Valle",
-		"last_name" : "Gonzalez",
-		"email" : "lore1@gmail.com",
-		"phone_number" :"7771338587",
-		"username":"lore123",
-		"password" :"1234",
-		"id_user_type": 2
-	}`
 )
 
 func TestCreateUser(t *testing.T) {
 
 	t.Run("Create User succesfully", func(t *testing.T) {
-		//GIVEN
+		// GIVEN
+		user := domain.User{
+			Name:        "Flora",
+			MiddleName:  "Lopez",
+			LastName:    "Gonzalez",
+			Email:       "flolg@gmail.com",
+			PhoneNumber: "7771234567",
+			Username:    "flo18",
+			Password:    "hola123",
+			IDUserType:  1,
+		}
+
+		// Server
 		e := echo.New()
-		// Create a request to pass to our handler.
-		request := httptest.NewRequest(http.MethodPost, "/crear/usuario", strings.NewReader(userJSON))
-		request.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-		//Create a ResponseRecorder to record the response.
+		userJSON, err := json.Marshal(user)
+		assert.NoError(t, err)
+
+		request := httptest.NewRequest(http.MethodPost, "/crear/usuario", strings.NewReader(string(userJSON))) // Create a request to pass to our handler.
+		request.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)                                   //Create a ResponseRecorder to record the response.
 		recorder := httptest.NewRecorder()
-
 		echoContext := e.NewContext(request, recorder)
-		userController := InitUserController()
 
-		//THEN
-		err := userController.Create(echoContext)
+		// Database
+		db, mock, err := sqlmock.New()
+		assert.NoError(t, err)
+		defer db.Close()
+		userRepository := repository.InitializeUserRepository(db)
 
-		//EXPECT
+		mock.ExpectExec(`INSERT INTO USUARIO`).
+			WithArgs(user.Name, user.MiddleName, user.LastName, user.Email, user.PhoneNumber, user.Username,
+				user.Password, user.IDUserType).
+			WillReturnResult(sqlmock.NewResult(1, 1))
+
+		// WHEN
+		err = InitUserController(userRepository).Create((echoContext))
+
+		// THEN
 		if assert.NoError(t, err) {
 			assert.Equal(t, http.StatusCreated, recorder.Code)
+
+			expectedControllerMessage := ControllerMessageResponse{
+				StatusCode: http.StatusCreated,
+				Message:    fmt.Sprintf("Creater %v successfully", user.Username),
+			}
+			expectedBody, err := json.Marshal(expectedControllerMessage)
+			assert.Nil(t, err)
+
+			assert.Contains(t, recorder.Body.String(), string(expectedBody))
 		}
 	})
+
+	t.Run("Create user fails due to invalid JSON", func(t *testing.T) {
+		// GIVEN
+		e := echo.New()
+		request := httptest.NewRequest(http.MethodPost, "/crear/usuario", strings.NewReader(`{"}`))
+		request.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		recorder := httptest.NewRecorder()
+		echoContext := e.NewContext(request, recorder)
+
+		// WHEN
+		err := InitUserController(nil).Create((echoContext))
+
+		// THEN
+		assert.Nil(t, err)
+		assert.Equal(t, http.StatusInternalServerError, recorder.Result().StatusCode)
+	})
+}
+
+func TestUpdateUser(t *testing.T) {
+
 }
