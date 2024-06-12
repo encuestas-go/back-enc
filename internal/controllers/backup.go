ab071135f28c393eb22e6232c3d5be40e256fab6
Lorena Valle <lore1268valle@gmail.com>
2024-06-12 16:19:03 -0600

    Createf a Database Backup and Moving it to the Backup Directory (#31)
     Created a Database Backup and Moving it to the Backup Directory

--- internal/controllers/backup.go
+++ internal/controllers/backup.go
@@ -2,9 +2,12 @@ package controllers
 
 import (
 	"io/fs"
+	"log"
 	"net/http"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"time"
 
 	"github.com/labstack/echo/v4"
 )
@@ -24,13 +27,28 @@ type BackupFiles struct {
 }
 
 func (b *BackupController) Create(c echo.Context) error {
-	//date := "2024-06-09"
-	return nil
+	currentDate := time.Now().Format("2006-01-02")
+
+	zipFileName := currentDate + "_database.zip"
+
+	command := "zip -r " + zipFileName + " mysql-data && mv " + zipFileName + " backups/"
+
+	cmd := exec.Command("sh", "-c", command)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+
+	return c.JSON(http.StatusCreated, ControllerMessageResponse{
+		StatusCode: http.StatusCreated,
+		Message:    "Backup file successfully created",
+	})
 }
 
 func (b *BackupController) Get(c echo.Context) error {
-	//backupsDir := "../backups"
-	backupsDir := filepath.Join("internal", "backups")
+	backupsDir := filepath.Join("backups")
 
 	dir, err := os.ReadDir(backupsDir)
 	if err != nil {
