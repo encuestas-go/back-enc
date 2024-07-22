47f2a3687ad54b542c45c0dbf7e1eb58268b4ba8
Christian Hernandez Najera <chrismatla68@gmail.com>
2024-07-22 01:46:29 -0600

    fixed

--- backups/init.sh
+++ backups/init.sh
@@ -4,18 +4,25 @@ set -e
 # Define el archivo SQL que se va a ejecutar
 SQL_FILE=${SQL_FILE:-"backup.sql"}
 
+# Función para verificar si MySQL está listo
+mysql_ready() {
+    mysqladmin ping -h"127.0.0.1" -uroot -proot --silent
+}
+
 # Verifica si el archivo SQL existe
 if [ -f "/docker-entrypoint-initdb.d/$SQL_FILE" ]; then
-  echo "Esperando a que MySQL se inicie..."
+    echo "Esperando a que MySQL se inicie..."
 
-  # Esperar a que MySQL esté listo
-  while ! mysqladmin ping -h"localhost" --silent; do
-    sleep 1
-  done
+    # Esperar a que MySQL esté listo
+    while !(mysql_ready)
+    do
+        echo "Esperando a que MySQL se inicie..."
+        sleep 2
+    done
 
-  echo "Restaurando desde $SQL_FILE..."
-  mysql -uroot -proot ENCUESTA < "/docker-entrypoint-initdb.d/$SQL_FILE"
+    echo "Restaurando desde $SQL_FILE..."
+    mysql -h127.0.0.1 -uroot -proot ENCUESTA < "/docker-entrypoint-initdb.d/$SQL_FILE"
 else
-  echo "Archivo SQL $SQL_FILE no encontrado!"
-  exit 1
+    echo "Archivo SQL $SQL_FILE no encontrado!"
+    exit 1
 fi
