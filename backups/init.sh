6114fee614602e292fada6c6797e84b194a0649a
Christian Hernandez Najera <chrismatla68@gmail.com>
2024-07-22 01:42:30 -0600

    fixed

--- backups/init.sh
+++ backups/init.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 set -e
 
-# Define el archivo SQL a restaurar usando la variable de entorno SQL_FILE
+# Define el archivo SQL que se va a ejecutar
 SQL_FILE=${SQL_FILE:-"backup.sql"}
 
 # Verifica si el archivo SQL existe
 if [ -f "/docker-entrypoint-initdb.d/$SQL_FILE" ]; then
+  echo "Esperando a que MySQL se inicie..."
+
+  # Esperar a que MySQL esté listo
+  while ! mysqladmin ping -h"localhost" --silent; do
+    sleep 1
+  done
+
   echo "Restaurando desde $SQL_FILE..."
   mysql -uroot -proot ENCUESTA < "/docker-entrypoint-initdb.d/$SQL_FILE"
 else
